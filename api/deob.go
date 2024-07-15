cf7531b9d7f20233fdd161f88013c77e85b392aa
Amarnath C <75058759+xAmarnath@users.noreply.github.com>
2024-07-15 14:37:32 +0530

    Create deob.go

---
+++ api/deob.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"os/exec"
+)
+
+func Deobfuscate(w http.ResponseWriter, r *http.Request) {
+	key_id := r.URL.Query().Get("key_id")
+	file_id := r.URL.Query().Get("file_id")
+
+	if key_id == "" || file_id == "" {
+		http.Error(w, "Invalid parameters", http.StatusBadRequest)
+		return
+	}
+
+	cmd := exec.Command("./playplay", key_id, file_id)
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		http.Error(w, "Failed to deobfuscate", http.StatusInternalServerError)
+		return
+	}
+
+	resp := map[string]string{
+		"resp_key": string(out),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
